adc68037e056f619cd9e159ea9bf65301762b1a3
ayumu0106 <a.yamashita0106@gmail.com>
2024-11-15 15:59:50 +0900

    a

--- rails/sample_app/app/controllers/lists_controller.rb
+++ rails/sample_app/app/controllers/lists_controller.rb
@@ -1,7 +1,14 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
 
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+  
   def index
   end
 
