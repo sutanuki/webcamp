bb8153ccd56a748b8c6d646ac29cf7d4b7a1cb7f
ayumu0106 <a.yamashita0106@gmail.com>
2024-11-20 14:07:06 +0900

    a

---
+++ 第2課題/Bookers1/app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      render :new
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  def index
+    @books = Book.all  
+  end
+
+  def show
+    @book = Book.find(params[:id])  
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+end
